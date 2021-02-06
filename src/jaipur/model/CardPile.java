17fbbf7ba983b0967631d6b936491f1209df5b01
BananaOrange <785077053@qq.com>
2021-02-06 21:38:25 +0800

    [Desc] 拟定常用命令格式

--- src/jaipur/model/CardPile.java
+++ src/jaipur/model/CardPile.java
@@ -1,10 +1,12 @@
 package jaipur.model;
 
+import java.util.Queue;
+
 /**
  * 牌堆类，保存每类牌的相关信息
  */
 public class CardPile {
     private int jewelleryNum = 6;
     private int goldenNum = 6;
-    private int
+
 }
---
+++ src/jaipur/model/CoinsPile.java
@@ -0,0 +1,16 @@
+package jaipur.model;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 货物代币堆
+ */
+public class CoinsPile {
+
+    private Queue<Integer> jewelleryCoins = new LinkedList<Integer>();
+
+    public CoinsPile(){
+        jewelleryCoins.
+    }
+}
